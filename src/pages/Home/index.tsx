92d92b74af0c0355b34787fae91cbf212ef4ed0a
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-26 10:53:37 -0300

    Desenvolvendo cabecalho da pagina inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,28 @@
+import icon from "../../assets/images/home-page/icon.png";
+
+import "./style.scss";
+
 function HomePage()
 {
   return (
     <div className="home-page">
-      <h1>Home</h1>
+      <nav>
+        <a className="home-link" href="#" >
+          <img className="icon" src={icon}/>
+        </a>
+
+        <ul className="menu-list">
+          <li><a className="" href="#">Solicitações de Adoção</a></li>
+          <li><a className="" href="#">ONGs</a></li>
+          <li><a className="active" href="#">Banco de Adoção</a></li>
+          <li><a className="" href="#">Minha Conta</a></li>
+          <li><a className="" href="#">Sair</a></li>
+        </ul>
+      </nav>
+
+      <div className="adoption-offers-section">
+        <h2 className="title">Banco de Adoção</h2>
+      </div>
     </div>
   );
 }
