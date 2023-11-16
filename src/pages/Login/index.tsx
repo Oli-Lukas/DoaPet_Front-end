77c94ce2ead90e140357cc7d51ba0d495043854e
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-11-16 10:51:21 -0300

    Implementando login do usuario
    Apos uma tentiva bem-sucedida de login, o usuario sera redirecionado
    para a pagina inicial da plataforma.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,56 @@
-import { Link } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import LoginIcon from "../../assets/images/login-page/login-icon.png";
 import "./style.scss";
+import { api } from "../../lib/axios";
 
 function Login()
 {
+  const navigator = useNavigate();
+
+  const [login, setLogin]       = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function makeLogin(event: FormEvent<HTMLFormElement | EventTarget>)
+  {
+    event.preventDefault();
+
+    const response = await api.post("auth/login", { email: login, senha: password });
+
+    if (response.status === 200)
+    {
+      localStorage.setItem("doapet-user-token", response.data.token);
+      navigator("/home");
+    }
+    else
+    {
+      console.log("Erro ao efetuar autenticação!"); 
+    }
+  }
+
   return (
     <div className="login-page">
       <div className="login-container">
         <h2 className="brand-title">DoaPet</h2>
         <img className="login-icon" src={LoginIcon} title="" />
 
-        <form>
-          <input className="login" type="text" placeholder="Login"/>
-          <input className="password" type="password" placeholder="Senha"/>
+        <form onSubmit={makeLogin}>
+          <input
+            className="login"
+            type="text"
+            placeholder="Login"
+            onChange={(e) => setLogin(e.target.value)}
+            value={login}
+          />
+          <input
+            className="password"
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
-          <input type="submit" value="Entrar" />
+          <input type="submit" value="Entrar"/>
         </form>
 
         <p className="text first">
